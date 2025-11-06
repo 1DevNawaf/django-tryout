a01bf408554a592866854b8ac34a38e87ba81c3b
nawafalghodyah <nalghodyah@barq.com>
2025-11-06 13:34:22 +0300

    Making a model class, and migrate it to the db

--- Movies/MoviesList/models.py
+++ Movies/MoviesList/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=100, help_text="Enter a Movie name")
+    date = models.DateField(help_text="Enter a Movie date")
