85f87414fac07c444f13dda1afa51ec526014140
nawafalghodyah <nalghodyah@barq.com>
2025-11-07 16:35:44 +0300

    Function Based View

--- Movies/MoviesList/views.py
+++ Movies/MoviesList/views.py
@@ -1,7 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
 def Movie_Name(request):
     name = request.GET.get('name') or "Harry Potter"
     return render(request, "base.html", {"name":name})
 
 
+def welcome_page(request):
+    message = (f"<html> <h1> Welcome to the Movies List </h1> "
+              f"<p> MoviesList has {Movie.objects.count()} Movies</p>"
+              f"</html>")
+
+    return HttpResponse(message)
