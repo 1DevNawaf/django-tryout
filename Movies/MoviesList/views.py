64cb0b1978bb76996102d636cf668694c4148242
nawafalghodyah <nalghodyah@barq.com>
2025-11-07 16:56:42 +0300

    Making a template

--- Movies/MoviesList/views.py
+++ Movies/MoviesList/views.py
@@ -2,14 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Movie
 
-def Movie_Name(request):
-    name = request.GET.get('name') or "Harry Potter"
-    return render(request, "base.html", {"name":name})
-
-
 def welcome_page(request):
-    message = (f"<html> <h1> Welcome to the Movies List </h1> "
-              f"<p> MoviesList has {Movie.objects.count()} Movies</p>"
-              f"</html>")
-
-    return HttpResponse(message)
+    return render(request, 'base.html')
